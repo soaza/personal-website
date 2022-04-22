efa4019f9a9ad5c3bf943d158f992a0a4ba561db
soaza <65228562+soaza@users.noreply.github.com>
2022-04-22 16:20:07 +0800

    Add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
 import type { NextPage } from "next";
 import { About } from "../components/About";
 import { Section } from "../components/Section";
-import Scrollspy from "react-scrollspy";
 import { SECTIONS } from "../common/constants";
 import { ScrollDetector } from "../components/ScrollDetector";
 import { Border } from "../components/Border";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title> {"Kim Guan | dev"}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
       <ScrollDetector />
 
       <About />
