b11c50c2aa25f70acf032655c823ee3670289f85
soaza <chukimguan@gmail.com>
2023-04-17 14:05:29 +0800

    make spotify api refreshable

--- pages/api/spotify.ts
+++ pages/api/spotify.ts
@@ -2,13 +2,13 @@
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ISpotifyRecentlyPlayedTrack } from "../../common/interfaces";
+import { spotifyService } from "../../services";
 
 type Data = { recentlyPlayedTrack: ISpotifyRecentlyPlayedTrack };
 
-/*https://accounts.spotify.com/authorize?client_id=2c94eb5645d2425c9c81e86ad93b51d6&
-response_type=code&redirect_uri=https://kimguanified.me&
-scope=user-read-playback-state
-*/
+/*https://accounts.spotify.com/authorize?client_id=2c94eb5645d2425c9c81e86ad93b51d6&response_type=code&redirect_uri=https://kimguanified.me&scope=user-read-recently-played
+ */
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
@@ -35,20 +35,8 @@ export default async function handler(
 
   //   const { access_token } = response.data;
   //   console.log(access_token);
-  const access_token =
-    "BQDoPUsZB5wzxcXNXzgOQCDnIdJ6_OMhaGaXF2Ynwj5SnvR1hhoq0VosAtPeqOyB-suoAJkbTVtwTZk7ek8fRcAMVt4aXOgvpNt21exzUzWsvxZPsvs3-3viu44BSodiEv69GAkYhQQ-nCCij1fKYgwFNY5qX5NottgxYpxJbNBsJ4g4AA";
+  await spotifyService.getNewAccessTokenFromRefreshToken();
+  const songsRes = await spotifyService.getRecentlyPlayedSongs();
 
-  const spotifyRes = await axios
-    .get<{ items: ISpotifyRecentlyPlayedTrack[] }>(
-      "https://api.spotify.com/v1/me/player/recently-played",
-      {
-        headers: { Authorization: "Bearer " + access_token },
-      }
-    )
-    .catch((err) => {
-      console.log(err.response.data);
-      throw new Error(err.response.data);
-    });
-
-  res.status(200).json({ recentlyPlayedTrack: spotifyRes.data.items[0] });
+  res.status(200).json({ recentlyPlayedTrack: songsRes.data.items[0] });
 }
