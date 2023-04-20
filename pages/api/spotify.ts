32d57cd6d0449830422759eaf9449b90c01b22e3
soaza <chukimguan@gmail.com>
2023-04-20 15:57:37 +0800

    add framer motion animations

--- pages/api/spotify.ts
+++ pages/api/spotify.ts
@@ -13,28 +13,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  //   const params = new URLSearchParams();
-
-  //   params.append("client_id", "2c94eb5645d2425c9c81e86ad93b51d6");
-  //   params.append("client_secret", "412775047fd74588ba97b58e51ee1402");
-  //   params.append("grant_type", "authorization_code");
-  //   params.append(
-  //     "code",
-  //     "AQDNv2Q8EW50EHHZZ5tkIvAav78MQoLLLXr6qro7wJGhzZJ9m0NcczFdIQ75FSRslJnciGlm_kVZcSmiwpUg2yidRxUzI9BFI6BFJqrXnHOi0gXPcDhlFv1lZVZKws25Y53S1MpT0TMaMLSWzx7MEPpcK3Iv6gIgChST4J4J8YcE0-E9Z4RQ__pJfXO7CThjijPPPMqv"
-  //   );
-  //   params.append("redirect_uri", "https://kimguanified.me");
-
-  //   const response = await axios({
-  //     url: "https://accounts.spotify.com/api/token",
-  //     method: "post",
-  //     params,
-  //   }).catch((err) => {
-  //     console.log(err.response.data);
-  //     throw new Error(err.response.data);
-  //   });
-
-  //   const { access_token } = response.data;
-  //   console.log(access_token);
   await spotifyService.getNewAccessTokenFromRefreshToken();
   const songsRes = await spotifyService.getRecentlyPlayedSongs();
 
