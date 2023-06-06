db9e5d5fbe5cad74ec9cc13b2512203797fb8bd3
soaza <chukimguan@gmail.com>
2023-06-06 16:28:21 +0800

    add supabase to update movies

--- components/Boxes/MovieBox.tsx
+++ components/Boxes/MovieBox.tsx
@@ -1,23 +1,47 @@
-import axios from "axios";
 import React from "react";
 import { useQuery } from "react-query";
-import { ISpotifyRecentlyPlayedTrack } from "../../common/interfaces";
 import { Box } from "../Box";
+import { supabase } from "../../common/supabase";
+
+interface IMovie {
+  created_at: Date;
+  movie_name: string;
+  movie_director: string;
+  movie_image: string;
+}
 
 export const MovieBox = () => {
+  const { data: movieData } = useQuery<IMovie | undefined>(
+    "recently-watched-movie",
+    async () => {
+      const { data, error } = await supabase
+        .from("movies")
+        .select()
+        .order("created_at", { ascending: false });
+
+      if (!data) {
+        return;
+      }
+
+      return data[0] as IMovie;
+    }
+  );
+
   return (
     <Box className=" bg-gray-800">
       <>
         <img
-          src={"https://images5.alphacoders.com/119/thumb-1920-1198137.jpg"}
-          className="w-full h-full absolute top-0  left-0 rounded-3xl opacity-40 -z-10"
+          src={movieData?.movie_image}
+          className="w-full h-full absolute top-0 left-0 rounded-3xl opacity-40 -z-10 object-cover"
         />
 
         <div className="text-white flex flex-col gap-2 h-full">
-          <div className="text-4xl 2xl:text-3xl font-bold ">Suzume</div>
+          <div className="text-4xl 2xl:text-3xl font-bold ">
+            {movieData?.movie_name}
+          </div>
 
           <div className="text-2xl font-thin 2xl:text-2xl text-orange-300">
-            Makoto Shinkai
+            {movieData?.movie_director}
           </div>
 
           <a className="absolute bottom-2 right-2 kist flex flex-row align-middle items-center gap-2">
