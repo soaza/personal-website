5a518b292a816839640d2bff5dee1160f2dc516d
soaza <chukimguan@gmail.com>
2023-06-06 17:02:41 +0800

    remove react-reveal

--- components/Works.tsx
+++ components/Works.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { WORK_EXPERIENCES } from "../common/constants";
 import { WorkExperience } from "./WorkExperience";
-const Fade = require("react-reveal/Fade");
 
 export const Works = () => {
   return (
     <>
       {WORK_EXPERIENCES.map((work, index) => {
-        return (
-          <Fade key={index} bottom>
-            <WorkExperience key={index} work={work} />
-          </Fade>
-        );
+        return <WorkExperience key={index} work={work} />;
       })}
     </>
   );
